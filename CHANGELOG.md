52021090b61efdcc7f19507dfafbc9f68e45dacf
Chen Yi-Cyuan <emn178@gmail.com>
2016-04-30 15:54:36 +0800

    Support mobile device

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v0.1.1 / 2016-04-30
+
+Support mobile device
+
 # v0.1.0 / 2015-11-14
 
 Create project
--- src/jquery.clickout.js
+++ src/jquery.clickout.js
@@ -1,35 +1,32 @@
-/*
- * jQuery-clickout v0.1.0
- * https://github.com/emn178/jquery-clickout
+/**
+ * [jQuery-clickout]{@link https://github.com/emn178/jquery-clickout}
  *
- * Copyright 2015, emn178@gmail.com
- *
- * Licensed under the MIT license:
- * http://www.opensource.org/licenses/MIT
+ * @version 0.1.1
+ * @author Chen, Yi-Cyuan [emn178@gmail.com]
+ * @copyright Chen, Yi-Cyuan 2015-2016
+ * @license MIT
  */
-;(function($, undefined) {
+(function ($) {
   'use strict';
 
   function bind(handleObj) {
     var element = $(this);
-    var onClickDocument = function(e) {
+    var onClickDocument = function (e) {
       if (!$.contains(element[0], e.target)) {
-        handleObj.handler.call(element, jQuery.Event('clickout', {target:e.target}));
+        handleObj.handler.call(element, $.Event('clickout', { target: e.target }));
       }
     };
     handleObj.clickout = onClickDocument;
-    $(document).bind('click', onClickDocument);
+    $(document).bind('click touchend', onClickDocument);
   }
 
   function unbind(handleObj) {
-    $(document).unbind('click', handleObj.clickout);
+    $(document).unbind('click touchend', handleObj.clickout);
   }
 
   $.event.special.clickout = { add: bind, remove: unbind };
 
-  $.fn.clickout = function( data, fn ) {
-    return arguments.length > 0 ?
-      this.bind( 'clickout', data, fn ) :
-      this.trigger( 'clickout' );
+  $.fn.clickout = function (data, fn) {
+    return arguments.length > 0 ? this.bind('clickout', data, fn) : this.trigger('clickout');
   };
 })(jQuery);
